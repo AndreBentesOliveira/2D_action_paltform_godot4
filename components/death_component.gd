caae3940c082ff7cd4b77b2240fb683bd1678d69
Andre Bentes de Oliveira <maciota2000@gmail.com>
2025-10-17 17:27:35 -0500

    ajustes17_10_2

--- components/death_component.gd
+++ components/death_component.gd
@@ -4,9 +4,12 @@ extends Node3D
 @export var sprite : AnimatedSprite3D
 
 func _ready() -> void:
+	if not sprite == null:
+		$GPUParticles3D.texture = sprite.texture
 	if not health_component == null:
 		health_component.die.connect(on_died)
 
 
 func on_died():
 	var entities = get_tree().get_first_node_in_group("entities_layer")
+	get_parent().remove_child(self)
