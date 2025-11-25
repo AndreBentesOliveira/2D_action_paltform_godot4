1c6d16a12d0f359dbb339492c00d49938eaec9b7
Andre <maciota2000@gmail.com>
2025-11-24 21:02:53 -0500

    BU COM JUMP BUFFER RESOLVIDO

--- entities/Player/states/fall.gd
+++ entities/Player/states/fall.gd
@@ -14,7 +14,9 @@ func _enter_state(_old_state: StringName, _params: Dictionary) -> void:
 
 func _physics_process(delta: float) -> void:
 	super(delta)
-	if player.is_on_floor():
+	if player.velocity.y > 0.0:
+		return enter_state(&"Jump")
+	if player.velocity.y == 0.0:
 		if player.velocity.x != 0.0:
 			return enter_state(&"Walk")
 		else:
