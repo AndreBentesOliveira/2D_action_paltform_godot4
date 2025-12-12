fb5b90fd25ce7234cf0445e4d89d914335e3321c
Andre Bentes de Oliveira <maciota2000@gmail.com>
2025-12-12 17:17:44 -0500

    mexi nas particulas

--- components/particles_emitter.gd
+++ components/particles_emitter.gd
@@ -2,11 +2,11 @@ extends Marker3D
 
 
 @onready var jump_particles = preload("res://effects/jump_particles.tscn")
-
+@onready var grab_particles = preload("res://effects/grab_particles.tscn")
 
 func emitte():
 	var entities = get_tree().get_first_node_in_group("entities_layer")
-	var jp = jump_particles.instantiate()
+	var jp = grab_particles.instantiate()
 	entities.add_child(jp)
 	jp.global_position = self.global_position
 	jp.emitting = true
